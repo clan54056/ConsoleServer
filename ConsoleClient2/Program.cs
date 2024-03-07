d6dbc9114e5fa8cddccce4b694e2eef976792a6b
clan54056 <128791046+clan54056@users.noreply.github.com>
2024-03-07 15:11:51 +0100

    Add project files.

---
+++ ConsoleClient2/Program.cs
@@ -0,0 +1,102 @@
+﻿// A C# program for Client
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+
+    class Program
+    {
+
+        // Main Method
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+            Console.ReadLine();
+        }
+
+        // ExecuteClient() Method
+        static void ExecuteClient()
+        {
+
+            try
+            {
+
+                // Establish the remote endpoint 
+                // for the socket. This example 
+                // uses port 11111 on the local 
+                // computer.
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+                // Creation TCP/IP Socket using 
+                // Socket Class Constructor
+                Socket sender = new Socket(ipAddr.AddressFamily,
+                        SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(localEndPoint);
+                    Console.WriteLine("Socket connected to -> {0}", sender.LocalEndPoint.ToString());
+
+                    // Start a separate thread for receiving messages
+                    new Thread(() =>
+                    {
+                        while (true)
+                        {
+                            byte[] messageReceived = new byte[1024];
+                            int byteRecv = sender.Receive(messageReceived);
+                            if (byteRecv == 0)
+                            {
+                                // Server disconnected
+                                Console.WriteLine("Server disconnected.");
+                                break;
+                            }
+
+                            Console.WriteLine("Message from Server -> {0}", Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
+                        }
+                    }).Start();
+
+                    // Send and receive messages in a loop
+                    while (true)
+                    {
+                        string message = Console.ReadLine();
+                        if (message.ToLower() == "exit")
+                            break;
+
+                        byte[] messageSent = Encoding.ASCII.GetBytes(message);
+                        int byteSent = sender.Send(messageSent);
+                    }
+                }
+
+                // Manage of Socket's Exceptions
+                catch (ArgumentNullException ane)
+                {
+
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ ConsoleServer/Program.cs
@@ -0,0 +1,115 @@
+﻿// A C# Program for Server
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+
+    class Program
+    {
+
+        // Main Method
+        static void Main(string[] args)
+        {
+            ExecuteServer();
+        }
+
+        public static void ExecuteServer()
+        {
+
+            bool wer = true;
+            bool ert = true;
+
+            // Establish the local endpoint 
+            // for the socket. Dns.GetHostName
+            // returns the name of the host 
+            // running the application.
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            // Creation TCP/IP Socket using 
+            // Socket Class Constructor
+            Socket listener = new Socket(ipAddr.AddressFamily,
+                        SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                // Using Bind() method we associate a
+                // network address to the Server Socket
+                // All client that will connect to this 
+                // Server Socket must know this network
+                // Address
+                listener.Bind(localEndPoint);
+
+                // Using Listen() method we create 
+                // the Client list that will want
+                // to connect to Server
+                listener.Listen(10);
+
+                List<Socket> clients = new List<Socket>();
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connection...");
+
+                    Socket clientSocket = listener.Accept();
+                    clients.Add(clientSocket);
+
+                    // Create a new thread to handle the client
+                    new System.Threading.Thread(() =>
+                    {
+                        while (true)
+                        {
+                            byte[] bytes = new Byte[1024];
+                            string data = null;
+
+                            try
+                            {
+                                int numByte = clientSocket.Receive(bytes);
+
+                                if (numByte == 0)
+                                {
+                                    // Client disconnected
+                                    clients.Remove(clientSocket);
+                                    break;
+                                }
+
+                                data = Encoding.ASCII.GetString(bytes, 0, numByte);
+                                Console.WriteLine("Received from client: {0}", data);
+
+                                // Broadcast the message to all clients
+                                foreach (var otherClient in clients)
+                                {
+                                    if (otherClient != clientSocket)
+                                    {
+                                        otherClient.Send(Encoding.ASCII.GetBytes(data));
+                                    }
+                                }
+                            }
+                            catch (SocketException)
+                            {
+                                // Client disconnected
+                                clients.Remove(clientSocket);
+                                break;
+                            }
+                        }
+
+                        // Close client Socket
+                        clientSocket.Close();
+                    }).Start();
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
